bc79b8255154b475314db472ce03fa3426048ee3
Troy <Hoops021@gmail.com>
2012-05-17 06:48:21 -0400

    Initial Commit

---
+++ iOS-WebGL-Viewer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS-WebGL-Viewer
+//
+//  Created by Troy Ferrell on 5/17/12.
+//  Copyright (c) 2012 Troy Ferrell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
